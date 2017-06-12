b8ab92f0df8cbdd93cd46a67388dc4ed91cee2d0
Thanos Kontis <thanos.kontis@foodtecsolutions.com>
2017-06-12 10:49:50 -0400

    Hackerrank: maximum subset problem

---
+++ Playground/src/MaxSubset.java
@@ -0,0 +1,41 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaxSubset {
+
+    static int getMoneySpent(int[] keyboards, int[] drives, int funds) {
+	int[] distinctSortedDrivesDesc = Arrays.stream(drives).distinct().sorted().toArray();
+	int[] distinctSortedKeyboardsAsc = Arrays.stream(keyboards).distinct().sorted().toArray();
+	int moneySpent = 0;
+	for (int keyboardPrice : distinctSortedKeyboardsAsc) {
+	    for (int drivePrice : distinctSortedDrivesDesc) {
+		int comboPrice = keyboardPrice + drivePrice;
+		if (comboPrice > funds)
+		    break;
+		if (comboPrice == funds)
+		    return comboPrice;
+		if (moneySpent < comboPrice)
+		    moneySpent = comboPrice;
+	    }
+	}
+	return moneySpent;
+    }
+
+    public static void main(String[] args) {
+	Scanner in = new Scanner(System.in);
+	int s = in.nextInt();
+	int n = in.nextInt();
+	int m = in.nextInt();
+	int[] keyboards = new int[n];
+	for (int keyboards_i = 0; keyboards_i < n; keyboards_i++) {
+	    keyboards[keyboards_i] = in.nextInt();
+	}
+	int[] drives = new int[m];
+	for (int drives_i = 0; drives_i < m; drives_i++) {
+	    drives[drives_i] = in.nextInt();
+	}
+	//  The maximum amount of money she can spend on a keyboard and USB drive, or -1 if she can't purchase both items
+	int moneySpent = getMoneySpent(keyboards, drives, s);
+	System.out.println(moneySpent);
+    }
+}
