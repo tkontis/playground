795f393a394b7d584e813178639416866f4705ae
Thanos Kontis <thanos.kontis@gmail.com>
2018-01-16 22:06:16 +0200

    Trie data structure

---
+++ js/src/trie.js
@@ -0,0 +1,85 @@
+class Trie {
+  constructor(sentence = '') {
+    this.root = new Node();
+    this.addWords(sentence);
+  }
+
+  sanitizeWord(word) {
+    if (typeof word !== 'string' || typeof word !== 'number') return '';
+    return String(word).toLowerCase().replace(/[^a-z]/g, '');
+  }
+
+  addWord(word) {
+    const sanitizedWord = this.sanitizeWord(word);
+    if (!sanitizedWord.length) return false;
+    let node = this.root;
+    sanitizedWord.split('')
+      .forEach((letter, idx) => {
+        const isTerminalNode = idx === word.length - 1;
+        const nextNode = node.addChild(letter, isTerminalNode);
+        node = nextNode;
+      });
+    return true;
+  }
+
+  addWords(words) {
+    let tokens = null;
+    const warnings = [];
+    if (Array.isArray(words) && words.length) {
+      tokens = words
+        .filter(word => typeof(word)=='string' || typeof(word)=='number')
+        .map(word => String(word).toLowerCase());
+    } else if (typeof words === 'string') {
+      tokens = words.toLowerCase().split(/\W+/);
+    } else {
+      throw new TypeError(`Invalid argument type for "words": ${typeof(words)}`);
+    }
+    if (tokens) {
+      tokens
+        .filter((word, idx, arr) => arr.lastIndexOf(word))  // remove duplicates
+        .forEach(word => {
+          const added = this.addWord(word);
+          if (!added) {
+            warnings.push(word);
+          }
+        });
+    }
+    return warnings.length ? `The following tokens could not be added: ${warnings.join(', ')}` : null;
+  }
+
+  searchWord(word) {
+    let node = this.root;
+    const sanitizedWord = this.sanitizeWord(word);
+    const lastIndex = sanitizedWord.length - 1;
+    return sanitizedWord.split('').every((letter, idx) => {
+      node = node.getChild(letter);
+      return Boolean(node) && (idx < lastIndex || node.isTerminalNode);
+    });
+  }
+}
+
+class Node {
+  constructor({isTerminalNode = false, value = null} = {}) {
+    this.isTerminalNode = isTerminalNode;
+    this.value = value;
+    this.children = {};
+  }
+  hasChild(letter) {
+    return this.children.hasOwnProperty(letter);
+  }
+  getChild(letter) {
+    return this.children[letter] || null;
+  }
+  addChild(letter, isTerminalNode) {
+    let childNode = this.getChild(letter);
+    if (childNode) {
+      childNode.isTerminalNode |= isTerminalNode;
+    } else {
+      childNode = new Node({value: (this.value||'') + letter, isTerminalNode});
+      this.children[letter] = childNode;
+    }
+    return childNode;
+  }
+}
+
+module.exports = { Trie };
---
+++ js/test/trie.js
@@ -0,0 +1,43 @@
+const {Trie} = require('../src/trie');
+const {assert} = require('chai');
+
+describe('Trie data structure', () => {
+  let trie = null;
+
+  beforeEach(() => {
+    trie = new Trie();
+  });
+
+  it('should create an empty trie tree with an initialized root node', () => {
+    assert(trie.root);
+    assert.isNull(trie.root.value);
+    assert.deepEqual(trie.root.children, {});
+  });
+  
+  it('should create a populated trie tree with a string argument', () => {
+    const sentence = 'this is an interesting data structure';
+    trie = new Trie(sentence);
+    assert(trie.root);
+    sentence.split(' ').forEach(word => {
+      assert.isTrue(trie.searchWord(word));
+    });
+  });
+
+  it('should detect words case insensitively', () => {
+    const words = 'My name is Bond';
+    trie.addWords(words);
+    assert.isTrue(trie.searchWord('my'));
+    assert.isTrue(trie.searchWord('My'));
+    assert.isTrue(trie.searchWord('Bond'));
+    assert.isTrue(trie.searchWord('bond'));
+  });
+
+  it('should work with numbers as well', () => {
+    const numbers = '123 1234 58290 623 9124234789732';
+    trie = new Trie(numbers);
+    numbers.split('').map(Number).forEach(num => {
+      assert.isTrue(trie.searchWord(num));
+    });
+  });
+
+});
